cd332959f32a53feb3dd99352c746e7997969c59
aajaafa97 <ajaafar97@gmail.com>
2023-05-21 15:24:38 -0400

    Adding project page

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/projects')
+def projects():
+    project_data = [
+        {'name': 'Project 1', 'description': 'Redlining Detroit', 'link': 'https://github.com/aajaafar97/Redlining_Detroit'},
+        {'name': 'Project 2', 'description': 'Baseball players, degrees of seperation', 'link': 'https://github.com/aajaafar97/507finalaajaafar'},
+        # Add more projects as necessary...
+    ]
+    return render_template('projects.html', projects=project_data)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
